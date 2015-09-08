9a511385e0d87367a3f9081dcfd9a3dc5d2a994f
slq0378 <slq0378@163.com>
2015-09-08 15:16:12 +0800

    Initial Commit

---
+++ 05-QQ列表实现/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  05-QQ列表实现
+//
+//  Created by Christian on 15/9/8.
+//  Copyright © 2015年 slq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 05-QQ列表实现/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  05-QQ列表实现
+//
+//  Created by Christian on 15/9/8.
+//  Copyright © 2015年 slq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
