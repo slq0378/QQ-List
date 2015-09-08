923a33a7e1446319def739fca02c44ce385bccad
slq0378 <slq0378@163.com>
2015-09-08 15:21:36 +0800

    1、项目初始化完成

--- 05-QQ列表实现/Classes/Controller/MainViewController.h
+++ 05-QQ列表实现/Classes/Controller/MainViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  MainViewController.h
 //  05-QQ列表实现
 //
 //  Created by Christian on 15/9/8.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface MainViewController : UIViewController
 
 @end
-
---
+++ 05-QQ列表实现/Classes/Controller/MainViewController.m
@@ -0,0 +1,23 @@
+//
+//  MainViewController.m
+//  05-QQ列表实现
+//
+//  Created by Christian on 15/9/8.
+//  Copyright © 2015年 slq. All rights reserved.
+//
+
+#import "MainViewController.h"
+
+@interface MainViewController ()
+
+@end
+
+@implementation MainViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
